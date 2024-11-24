99b58f34c048ab157f6c67dc93ced987c8d7edc1
Dariana Narváez <167484697+DarianaUwu@users.noreply.github.com>
2024-11-23 23:18:56 -0600

    llamado SP Insertar

--- Modelo/SP.java
+++ Modelo/SP.java
@@ -23,4 +23,32 @@ public class SP {
     }
 
 
+    public boolean InsertarUsuario(String idUsuario, String primerNombre, String segundoNombre,
+    String primerApellido, String segundoApellido, String login,
+    String clave, java.sql.Date fechaCreacion) {
+
+     String sql = "{ CALL InsertarUsuario(?, ?, ?, ?, ?, ?, ?, ?) }";
+    try (CallableStatement stmt = conexion.prepareCall(sql)) {
+    stmt.setString(1, idUsuario);
+    stmt.setString(2, primerNombre);
+    stmt.setString(3, segundoNombre);
+    stmt.setString(4, primerApellido);
+    stmt.setString(5, segundoApellido);
+    stmt.setString(6, login);
+    stmt.setString(7, clave);
+    stmt.setDate(8, fechaCreacion);
+
+     int rowsInserted = stmt.executeUpdate();
+    return rowsInserted > 0; // Retorna true si se insertó el usuario
+    } catch (SQLException e) {
+     e.printStackTrace();
+   return false; // Retorna false si ocurre un error
+    }
+
+
+    
+}
+
+
+
 }
